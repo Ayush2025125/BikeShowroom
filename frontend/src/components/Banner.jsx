306091f3b80f40ac91b599a7a7c6f82d2f37aaff
Ayush <ayushraj2005.muz@gmail.com>
2025-08-08 18:37:45 +0530

    Optimized

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -25,12 +25,12 @@ const Banner = () => {
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                 </svg>
               </div>
-              <span className="font-bold">+91 XXXXXXXXXX</span>
+              <span className="font-bold">+91 9876543210</span>
             </div>
             
             {/* White Bordered Phone Button */}
             <div className="flex items-center bg-transparent border border-white text-white py-2 px-4 rounded-full">
-              <span className="font-bold mr-2">+91 XXXXXXXX5</span>
+              <span className="font-bold mr-2">+91 9182734567</span>
               <div className="bg-white rounded-full p-2">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-800" viewBox="0 0 20 20" fill="currentColor">
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
